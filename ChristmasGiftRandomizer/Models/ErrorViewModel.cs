4d008204ebd910cbad700ea5362266afa54c9b05
ramzy2000 <caseyduffy00@gmail.com>
2025-12-05 19:11:16 -0600

    Revert "Rework project into .net project"
    This reverts commit bd1a9a4151285ecc23782f4ee945f5287cdbeb7c.

--- ChristmasGiftRandomizer/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ChristmasGiftRandomizer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
